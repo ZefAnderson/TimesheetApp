3f07c3584cf61be1e5814264fd4155636305829f
Zef <bcdfezz@gmail.com>
2023-08-22 09:57:09 -0600

    unit 5 complete

--- hr-timesheet/src/app/components/departments/departments.component.ts
+++ hr-timesheet/src/app/components/departments/departments.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Department } from 'src/app/interfaces/department';
+import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
   selector: 'app-departments',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./departments.component.scss']
 })
 export class DepartmentsComponent {
+  departments!: Department[];
+
+  constructor(
+    private departmentsService: DepartmentsService,
+  ) { }
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+  }
 
 }
