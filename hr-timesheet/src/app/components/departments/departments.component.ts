2cd594a095a287771649ed3161664c5ef0045d38
Zef <bcdfezz@gmail.com>
2023-08-30 13:42:59 -0600

    Unit 12 done

--- hr-timesheet/src/app/components/departments/departments.component.ts
+++ hr-timesheet/src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Department } from 'src/app/interfaces/department';
 import { DepartmentsService } from 'src/app/services/departments.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -10,15 +11,18 @@ import { Router } from '@angular/router';
 })
 export class DepartmentsComponent {
   departments!: Department[];
+  $departments: Observable<Department[]> | undefined;
 
   constructor(
     private departmentsService: DepartmentsService,
     private router: Router
   ) { }
   ngOnInit(): void {
-    this.departmentsService.getDepartments().subscribe(departments => {
-      this.departments = departments;
-  });  }
+    // this.departmentsService.getDepartments().subscribe(departments => {
+    //   this.departments = departments;
+    // });  
+    this.$departments = this.departmentsService.getDepartments();
+  }
 
   goToDepartment(departmentId: string): void {
     this.router.navigate(['./timesheet', {id: departmentId}]);
